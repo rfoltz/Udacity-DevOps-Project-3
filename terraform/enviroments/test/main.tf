2a7104e6bf85515dab1553b3629d7c47cef96787
Robert Foltz <rfoltz512@gmail.com>
2021-03-14 18:10:41 -0400

    Scaffolding the modules folder and adding the first modules of resource group

--- terraform/enviroments/test/main.tf
+++ terraform/enviroments/test/main.tf
@@ -14,3 +14,8 @@ terraform {
     key                  = ""
   }
 }
+module "resource_group" {
+    source               ="../../modules/resource_group"
+    resource_group       =${var.resource_group}
+    location             =${var.location}
+}
